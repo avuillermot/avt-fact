ddbcb1b2fca7de0e3d852875f8d1a8a15199a3a7
alexandre.vuillermot <alexandre.vuillermot@kuehne-nagel.com>
2020-11-16 19:22:11 +0100

    Update a customer

--- src/controllers/customer.serv.ts
+++ src/controllers/customer.serv.ts
@@ -6,8 +6,20 @@ export class CustomerService {
         return back;
     }
 
-    public async get(entity: string): Promise<ICustomer[]> {
-        let customers = await Customer.find({ entityId: entity });
+    public async getAll(entity: string): Promise<ICustomer[]> {
+        let customers: ICustomer[];
+        customers = await Customer.find({ entityId: entity });
         return customers;
+    }
+
+    public async get(entity: string, id: string): Promise<ICustomer> {
+        let customer: ICustomer;
+        customer = <ICustomer> await Customer.findOne({ entityId: entity, _id: id });
+        return customer;
+    }
+
+    public async update(customer: ICustomer): Promise<ICustomer> {
+        let result: ICustomer = await Customer.updateOne({ _id: customer._id, entityId: customer.entityId }, customer);
+        return result;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import { ApplicationDbTestSettings as DbSettings, ApplicationSetting } from "./../src/config";
 import { CustomerService } from './controllers/customer.serv'
+import url = require('url');
+import bodyParser = require('body-parser');
+
 // rest of the code remains same
 const app = express();
 const PORT = 8000;
@@ -9,14 +12,31 @@ db.connection();
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Methods", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/', async(req, res) => {
+app.get('/customers', async(req, res) => {
     let serv: CustomerService = new CustomerService();
-    res.send(await serv.get("ENTTEST"));
+    const params: { entity: string, id: string } = <any>url.parse(req.url, true).query;
+    res.send(await serv.getAll(params.entity));
 });
+
+app.get('/customer', async (req, res) => {
+    let serv: CustomerService = new CustomerService();
+    const params: { entity: string, id: string } = <any> url.parse(req.url, true).query;
+    res.send(await serv.get(params.entity, params.id));
+});
+
+app.put('/customer', async (req, res) => {
+    let serv: CustomerService = new CustomerService();
+    await serv.update(req.body);
+    res.send();
+});
+
 app.listen(PORT, () => {
     console.log('[server]: Server is running at https://localhost:${PORT}');
 });
